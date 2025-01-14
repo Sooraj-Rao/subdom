06d44524d13a3d9596a7c173035f01d3ddb5458b
Sooraj-Rao <soorajrao630@gmail.com>
2025-01-14 13:17:38 +0530

    first commit2

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 export function middleware(req: Request) {
   const host = req.headers.get("host") || ""; // e.g., "testuser.localhost:3000"
   const subdomain = host.split(".")[0]; // Extract "testuser"
+  console.log("host", host);
+  console.log("subdomain", subdomain);
   // Ignore localhost and main domain
   if (subdomain !== "localhost" && subdomain !== "www") {
     const url = new URL(req.url);
